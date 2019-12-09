43e41f238e70e9607f9fa8009cece4de9bcf252a
Yam Clement <yamtzunhei@gmail.com>
2019-12-09 22:56:58 +0100

    created connection

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require("express");
 
+const mysql = require("mysql2");
+require("dotenv").config({ path: "./config/dev.env" });
+
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const connectionMySql = require("./db");
-
 // autorise tous à faire des requêtes
 app.use(cors());
 
-app.use(connectionMySql);
+// Connect to db
+
+const connectionMySql = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD_MYSQL,
+  database: process.env.DB
+});
+
+connectionMySql.connect(err => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log("Successfully connected to db");
+});
 
 // Permet d'utiliser req.body
 app.use(bodyParser.urlencoded({ extended: false }));
