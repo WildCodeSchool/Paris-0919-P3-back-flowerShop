2c0dfacda26dae0b8c93ab957924f498435f1bd6
Claire <clairepelletier.prod@gmail.com>
2020-01-10 11:02:26 +0100

    cluster linked

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import dotenv from 'dotenv';
 import mongodb from 'mongodb';
-//import unsafegames from './routes/unsafegames';
-// import unsafepublishers from './routes/unsafepublishers';
-// import games from './routes/games';
 import products from './routes/products';
-//import authgames from './routes/authgames';
 import users from './routes/users';
 import auth from './routes/auth';
 
@@ -19,21 +15,21 @@ const app = express();
 app.use('/static', express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.json());
 
-// routes
-//app.use('/api/unsafegames', unsafegames);
-// app.use('/api/unsafepublishers', unsafepublishers);
-// app.use('/api/games', games);
 app.use('/api/products', products);
-//app.use('/api/authgames', authgames);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 
-mongodb.MongoClient.connect(`${process.env.DB_CONNECTION}`, (err, db) => {
-  app.set('db', db);
-
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
-  });
-
-  app.listen(2370, () => console.log('Running on localhost:2370'));
-});
+mongodb.MongoClient.connect(
+  process.env.DB_CONNECTION,
+  {
+    useUnifiedTopology: true
+  },
+  (err, client) => {
+    const db = client.db('db');
+    app.set('db', db);
+    app.get('/*', (req, res) => {
+      res.sendFile(path.join(__dirname, './index.html'));
+    });
+    app.listen(2370, () => console.log('Running on localhost:2370'));
+  }
+);
